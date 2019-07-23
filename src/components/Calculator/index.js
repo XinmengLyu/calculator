fae6f8c87ede11ea509285b32f3d4f4db339194b
XinmengLyu <lvxinmeng_1995@163.com>
2019-07-23 14:12:31 -0400

    finished

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -17,14 +17,63 @@ class Calculator extends React.Component {
 
     callOperator = () => {
         console.log("call operator");
+        let { displayValue, selectedOperator, storedValue } = this.state;
+        const updateStoredValue = displayValue;
+        displayValue = parseInt(displayValue, 10);
+        storedValue = parseInt(storedValue, 10);
+
+        switch (selectedOperator) {
+            case "+": {
+                displayValue = storedValue + displayValue;
+                break;
+            }
+            case "-": {
+                displayValue = storedValue - displayValue;
+                break;
+            }
+            case "x": {
+                displayValue = storedValue * displayValue;
+                break;
+            }
+            case "/": {
+                displayValue = storedValue / displayValue;
+                break;
+            }
+            default: {
+                displayValue = 0;
+            }
+        }
+        displayValue = displayValue.toString();
+        selectedOperator = "";
+        if (displayValue === "NaN" || displayValue === "Infinity") displayValue = "0";
+
+        this.setState({
+            displayValue: displayValue,
+            selectedOperator: selectedOperator,
+            storedValue: updateStoredValue
+        });
     };
 
-    setOperator = () => {
+    setOperator = value => {
         console.log("set operator");
+        let { displayValue, selectedOperator, storedValue } = this.state;
+        if (selectedOperator === "") {
+            storedValue = displayValue;
+            displayValue = "0";
+            selectedOperator = value;
+        } else {
+            selectedOperator = value;
+        }
+
+        this.setState({
+            displayValue: displayValue,
+            selectedOperator: selectedOperator,
+            storedValue: storedValue
+        });
     };
 
     updateDisplay = value => {
-        console.log("update display");
+        console.log("update display", value);
         let { displayValue } = this.state;
         if (value === "." && displayValue.includes(".")) value = "";
 
